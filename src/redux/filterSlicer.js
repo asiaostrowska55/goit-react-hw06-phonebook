ab9184d65f264c2157aea561e4fbea8d24f4c0cd
asiaostrowska55 <110023821+asiaostrowska55@users.noreply.github.com>
2023-05-01 16:26:42 +0200

    cleanup

--- src/redux/filterSlicer.js
+++ src/redux/filterSlicer.js
@@ -9,22 +9,3 @@ export const filterReducer = createReducer(initialState, {
 });
 
 export default filterReducer;
-
-// import { createSlice } from '@reduxjs/toolkit';
-
-// const filterInitialState = {
-//   filter: '',
-// };
-
-// export const filterSlice = createSlice({
-//   name: 'filter',
-//   initialState: filterInitialState,
-//   reducers: {
-//     setFilter(action) {
-//       return action.payload;
-//     },
-//   },
-// });
-
-// export const { setFilter } = filterSlice.actions;
-// export const filterReducer = filterSlice.reducer;
