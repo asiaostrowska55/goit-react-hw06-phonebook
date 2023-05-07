b07b58c8f33a88f6e3bf4440834958ab9213e29c
asiaostrowska55 <110023821+asiaostrowska55@users.noreply.github.com>
2023-05-07 11:04:38 +0200

    local storage

--- src/redux/contactSlicer.js
+++ src/redux/contactSlicer.js
@@ -5,16 +5,20 @@ export const addContacts = createAction('contact/addContacts');
 export const deleteContacts = createAction('contact/deleteContacts');
 
 const initialState = () => {
-  return [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ];
+  const storedContacts = JSON.parse(localStorage.getItem('newContacts'));
+  return storedContacts
+    ? storedContacts
+    : [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      ];
 };
 export const contactsReducer = createReducer(initialState(), {
   [addContacts]: (state, action) => {
     state.push(action.payload);
+    localStorage.setItem('newContacts', JSON.stringify(state));
   },
   [deleteContacts]: (state, action) =>
     state.filter(({ id }) => id !== action.payload),
